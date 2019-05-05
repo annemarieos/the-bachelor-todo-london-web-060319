0341f39e278ad9df0add2a101ce8c920bdb4b772
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-05-05 16:30:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,19 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
+      return contestant_hash["name"].split(" ").first
+    end 
+  end   
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data[occupation].each do |contestant_hash|
+    if contestant_hash["occupation"] != nil 
+      return contestant_hash["name"].split(" ")
+    end 
+  end   
 end
 
 def count_contestants_by_hometown(data, hometown)
